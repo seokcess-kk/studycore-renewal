88d545f39f6766527e26786541d461d097dc6ac0
KYUSEOK SONG <seokcess@glitzy.kr>
2026-04-27 12:18:28 +0900

    chore: revert MetaPixel debug meta tag

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -26,11 +26,7 @@ export function MetaPixel() {
     window.fbq?.("track", "PageView");
   }, [pathname]);
 
-  if (!PIXEL_ID) {
-    return (
-      <meta name="meta-pixel-debug" content="PIXEL_ID is empty at build time" />
-    );
-  }
+  if (!PIXEL_ID) return null;
 
   const initScript = `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${PIXEL_ID}');fbq('track','PageView');`;
 
